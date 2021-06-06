c6898559aeed3937e5bc3e813c75090e60484005
Abhiram <abhiram.elf@gmail.com>
2021-06-06 23:10:50 +0530

    Personal portfolio website

---
+++ js/custom.js
@@ -0,0 +1,46 @@
+(function ($) {
+
+  "use strict";
+    // SEND EMAIL
+    $('.submit-btn').click(function(){
+        window.location.href = "mailto:abhiram@odynreality.com";
+    })
+
+    // DOWNLOAD CV
+    $('.download-cv').click(function(e){
+        e.preventDefault();
+        window.open('resources/Abhiram Resume.pdf');
+    })
+
+    // COLOR MODE
+    $('.color-mode').click(function(){
+        $('.color-mode-icon').toggleClass('active')
+        $('body').toggleClass('dark-mode')
+    })
+
+    // HEADER
+    $(".navbar").headroom();
+
+    // PROJECT CAROUSEL
+    $('.owl-carousel').owlCarousel({
+    	items: 1,
+	    loop:true,
+	    margin:10,
+	    nav:true
+	});
+
+    // SMOOTHSCROLL
+    $(function() {
+      $('.nav-link, .custom-btn-link').on('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 49
+        }, 1000);
+        event.preventDefault();
+      });
+    });  
+
+    // TOOLTIP
+    $('.social-links a').tooltip();
+
+})(jQuery);
